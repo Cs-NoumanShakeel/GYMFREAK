592213454f2e5073874ced98c0d603af4144a206
Nouman Shakeel <noumannorm648@gmail.com>
2025-08-18 22:19:27 +0500

    changing settings for cloudinary

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
+  
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
       # <-- required
     'api',
